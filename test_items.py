da0b82394260ba7cf7a11f0d4161f6af83fd8a23
Ann Bondar <479ani@gmail.com>
2023-03-09 18:35:26 +0800

    Add_importent_files_stepic_3.6

--- test_items.py
+++ test_items.py
@@ -1,12 +1,12 @@
 import pytest
 import time
 from selenium.webdriver.common.by import By
+import time
 
 links = [
     'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/',
     'http://selenium1py.pythonanywhere.com/catalogue/hackers-painters_185/'
 ]
-
 
 @pytest.mark.parametrize('link', links)
 def test_find_add_to_basket_button(browser, link):
